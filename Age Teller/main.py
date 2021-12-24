e3532d42c326c7d79e1ec2149b7815f914d3c30e
SidhakCodes <95403304+Metolix@users.noreply.github.com>
2021-12-24 21:41:25 +0530

    Small changes + Tic Tac Toe app

--- Age Teller/main.py
+++ Age Teller/main.py
@@ -1,4 +1,7 @@
+import time
 while True:
+    print("Welcome to Maahir Age teller!")
+    time.sleep(5)
     age = input("What is your age? ")
     final = 2021 - int(age)
     print(f"You were born in {final}")
--- Text to speech/main.py
+++ Text to speech/main.py
@@ -2,7 +2,7 @@ import pyttsx3
 
 while True:
 
-    line = input("What do you want the AI to say? ")
+    line = input("What do you want the AI to say?(Tip - If you want the bot to quit, say 'quit') ")
      
     if line == "quit":
         print("Quiting the program. Bye!")
